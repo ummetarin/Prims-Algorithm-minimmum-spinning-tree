298a82d3516ff5449eec18df92021c005971b831
ummetarin <ummehomairatarin@gmail.com>
2023-12-26 23:19:35 +0600

    Added minkey functions for  find minimmum key

--- PrimsMinimumsp.cpp
+++ PrimsMinimumsp.cpp
@@ -1,7 +1,32 @@
 #include<iostream>>
-using namespace std;
+#include<limits.h>
+#define V 5
+
+int minKey(int key[],bool setKey[]){
+    int min_value=INT_MAX;
+    int min_index;
+
+    // v=all vertex
+
+    for(int v=0;v<V;v++){
+      if(!setKey&&key[v]<min_value){
+        min_value=key[v];
+        min_index=v;
+
+      }
+
+    }
+    return min_index;
+
+    
+}
+
 
 int main(){
+
+
+
+
 
 
 
